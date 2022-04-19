1782e4a3415d2c179ee36a72de2ee63a0a3abe9f
WDav96 <molinadavidw20@gmail.com>
2022-04-19 17:37:48 -0500

    Initial Commit

---
+++ FreeFitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreeFitness
+//
+//  Created by Jorge Luis Rivera Ladino - Ceiba Software on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
